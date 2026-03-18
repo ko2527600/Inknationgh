4a02ecc5f181349f4266e5785933e66c6abee48d
kingsley okyere <ko2527600@gmail.com>
2026-03-18 11:22:11 +0000

    chore: use environment variables for frontend API URL and create .env.example

--- Ikenation-clothing/src/store/useAuthStore.js
+++ Ikenation-clothing/src/store/useAuthStore.js
@@ -2,7 +2,7 @@ import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 import axios from 'axios';
 
-const API_URL = `http://${window.location.hostname}:5000/api/auth`;
+const API_URL = `${import.meta.env.VITE_API_URL}/auth`;
 
 export const useAuthStore = create(
   persist(
--- Ikenation-clothing/src/store/useProductStore.js
+++ Ikenation-clothing/src/store/useProductStore.js
@@ -1,7 +1,7 @@
 import { create } from 'zustand';
 import axios from 'axios';
 
-const API_URL = `http://${window.location.hostname}:5000/api/products`;
+const API_URL = `${import.meta.env.VITE_API_URL}/products`;
 
 export const useProductStore = create((set, get) => ({
   products: [],
